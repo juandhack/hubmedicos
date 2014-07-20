de89c03b13358e52f27bdaf8b9012495b5c27895
Juan David Hernandez <juandhernandez2008@gmail.com>
2014-07-20 02:17:59 -0500

    otro

--- apps/inicio/urls.py
+++ apps/inicio/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import patterns, include, url
-from apps.inicio.views import sobre
+from .views import sobre
 
 urlpatterns = patterns('',
 
 	url(r'^$' , 'django.contrib.auth.views.login',
 		{'template_name':'inicio/index.html'}, name='login'),
 
-	url(r'^home/$','django.contrib.auth.views.login',
-		{'template_name':'inicio/blank-page.html'}, name='home'),
+	#url(r'^home/$','django.contrib.auth.views.login',
+		#{'template_name':'inicio/blank-page.html'}, name='home'),
 
-	url(r'^home/index/','django.contrib.auth.views.login',
-		{'template_name':'inicio/index.html'}, name='index'),
+	#url(r'^home/index/','django.contrib.auth.views.login',
+		#{'template_name':'inicio/index.html'}, name='index'),
+		url(r'^home/$','apps.inicio.views.sobre'),
 
 	
 )
